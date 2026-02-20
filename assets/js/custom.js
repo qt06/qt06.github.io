a7f3630187d18b9a4de01a971ac25c5c07926ce6
qt06 <qt06.com@gmail.com>
2026-02-20 16:32:20 +0800

    update post 420'

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -4,13 +4,9 @@ window.addEventListener("DOMContentLoaded", event => {
     });
     const getCurrentTheme = () => document.querySelector('html').dataset['theme'] == 'light' ? '当前是浅色' : '当前是深色';
     document.querySelectorAll('#theme-switcher').forEach(a => {
-        a.setAttribute('aria-label', '切换主题');
         a.setAttribute('aria-description', getCurrentTheme());
         a.addEventListener('click', function(e) {
             this.setAttribute('aria-description', getCurrentTheme());
         }, null);
-    });
-    document.querySelectorAll('#back-to-top a').forEach(a => {
-        a.setAttribute('aria-label', '返回顶部');
     });
 }, {once: true});
